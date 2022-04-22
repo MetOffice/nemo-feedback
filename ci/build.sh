3897f66bf208ad5f03a479a01c2b0bd6708083ad
twsearle <14909402+twsearle@users.noreply.github.com>
2022-04-22 21:08:26 +0100

    github actions config inspired by opsinputs

---
+++ ci/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+finally() {
+    trap '' ERR
+    trap '' EXIT
+    if [[ -d "${WORKD:-}" ]]; then
+        cd /
+        rm -fr "${WORKD}"
+    fi
+}
+
+HERE="$(cd "$(dirname "$0")" && pwd)"
+THIS="$(basename "$0")"
+NPROC=${NPROC:-2}
+WORKD="$(mktemp -d "${THIS}-XXXXXX" -t)"
+
+trap finally ERR
+trap finally EXIT
+
+cd "${WORKD}"
+
+rm -f "${HERE}/nemo-feedback"
+ln -s '..' "${HERE}/nemo-feedback"
+ecbuild -S "${HERE}" -DMPI_ARGS="--oversubscribe"
+make -j "${NPROC}"
+env OMPI_ALLOW_RUN_AS_ROOT=1 OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 \
+    ctest -j "${NPROC}" --output-on-failure --test-dir './nemo-feedback'
+
+exit
