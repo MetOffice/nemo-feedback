8c5c67a606c75ae0a20a50d858415b31c32dd2d6
twsearle <14909402+twsearle@users.noreply.github.com>
2022-10-07 18:28:55 +0100

    use ecbuild in ci script

--- ci/build.sh
+++ ci/build.sh
@@ -22,12 +22,13 @@ cd "${WORKD}"
 
 export LD_LIBRARY_PATH="${WORKD}/lib"
 export PLUGINS_MANIFEST_PATH="${WORKD}/share/plugins"
-export BUILD_DIR="build"
 
-cmake -S "${HERE}" -B "${BUILD_DIR}" -DCMAKE_BUILD_TYPE=Debug -DECBUILD_2_COMPAT="ON" -DMPI_ARGS="--oversubscribe"
-cmake --build "${BUILD_DIR}" -j "${NPROC}"
+rm -f "${HERE}/nemo-feedback"
+ln -s '..' "${HERE}/nemo-feedback"
+ecbuild -S "${HERE}" -DCMAKE_BUILD_TYPE=Debug -DMPI_ARGS="--oversubscribe"
+make -j "${NPROC}"
 
 env OMPI_ALLOW_RUN_AS_ROOT=1 OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 \
-   ctest -j "${NPROC}" -V --output-on-failure --test-dir "${BUILD_DIR}/nemo-feedback"
+   ctest -j "${NPROC}" -V --output-on-failure --test-dir "./nemo-feedback"
 
 exit
